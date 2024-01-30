63c96347f615e6258cc6e716477a516745cd6e72
bxgda <152297482+bxgda@users.noreply.github.com>
2024-01-30 21:51:37 +0100

    Add files via upload

--- Jun-2023.vhd
+++ Jun-2023.vhd
@@ -1,69 +1,57 @@
-entity INV is
-port (A : in bit; F : out bit);
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity oneHot is
+port(ulaz : in std_logic; Clock, Reset : in std_logic; izlaz : out std_logic);
 end entity;
-architecture INV_Arch of INV is
-begin
-F <= not A;
-end architecture;
 
-entity OR1 is
-port (A, B, C, D : in bit; F : out bit);
-end entity;
-architecture OR_Arch of OR1 is
-begin
-F <= A or B or C or D;
-end architecture;
-
-entity AND1 is
-port (A, B, C, D : in bit; F : out bit);
-end entity;
-architecture AND_Arch of AND1 is
-begin
-F <= A and B and C and D;
-end architecture;
-
-entity System1 is
-port (A, B, C, D : in bit; F : out bit);
-end entity;
-architecture Sys_arch of System1 is
-
-signal An, Bn, Cn, Dn : bit;
-signal m1, m2, m3, m4 : bit;
-
-component INV is 
-port (A : in bit; F : out bit);
-end component;
-
-component OR1 is
-port (A, B, C, D : in bit; F : out bit);
-end component;
-
-component AND1 is
-port (A, B, C, D : in bit; F : out bit);
-end component;
+architecture oneHot_b of oneHot is
+	subtype stanje is std_logic_vector (3 downto 0);		
+	constant s0 : stanje := "0001";
+	constant s1 : stanje := "0010";
+	constant s2 : stanje := "0100";
+	constant s3 : stanje := "1000";
+	signal trenutno, sledece : stanje;
 
 begin
 
-U1 : INV port map (A=>A, F=>An);
-U2 : INV port map (A=>B, F=>Bn);
-U3 : INV port map (A=>C, F=>Cn);
-U4 : INV port map (A=>A, F=>Dn);
+	memorija : process (Clock, Reset)				--proces za memoriju uvek isto
+	begin
+		if(Reset='0') then trenutno <= s0;
+		elsif(rising_edge(clock)) then trenutno <= sledece;
+		end if;
+	end process;
+	
+	prelaz : process (trenutno, ulaz)
+	begin
+		case(trenutno) is
+			when s0 => if(ulaz = '0') then sledece <= s1;
+			else sledece <= s2;
+			end if;
+			when s1 => if(ulaz = '0') then sledece <= s3;
+			else sledece <= s0;
+			end if;
+			when s2 => if(ulaz = '0') then sledece <= s3;
+			else sledece <= s0;
+			end if;
+			when s3 => if(ulaz = '0') then sledece <= s2;
+			else sledece <= s1;
+			end if;
+			when others => sledece <= s0;
+		end case;
+	end process; 
 
-U5 : AND1 port map (A=>An, B=>Bn, C=>Cn, D=>D, F=>m1);
-U6 : AND1 port map (A=>An, B=>Bn, C=>C, D=>D, F=>m2);
-U7 : AND1 port map (A=>A, B=>Bn, C=>Cn, D=>D, F=>m3);
-U8 : AND1 port map (A=>A, B=>Bn, C=>C, D=>D, F=>m4);
-
-U9 : OR1 port map (A=>m1, B=>m2, C=>m3, D=>m4, F=>F);
-
+	izlazi : process (trenutno)
+	begin
+		case(trenutno) is
+			when s0 => izlaz <= '1';
+			when others => izlaz <= '0';
+		end case;
+	end process;
 end architecture;
 
 
 
 
 
-
-
-
-
-
+		
