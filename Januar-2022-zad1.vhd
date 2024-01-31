eb6af032214bf37cc8beef5c6c2d7641ecb1c75e
bxgda <152297482+bxgda@users.noreply.github.com>
2024-02-01 00:18:56 +0100

    Update Januar-2022-zad1.vhd

--- Januar-2022-zad1.vhd
+++ Januar-2022-zad1.vhd
@@ -1,4 +1,4 @@
---demux1x8 konkurentna dodela
+--demux1x8 konkurentna dodela i proizvoljan TB
 
 entity demux_1to8 is
 port (A : in bit; Sel : in bit_vector(2 downto 0); F : out bit_vector(7 downto 0));
